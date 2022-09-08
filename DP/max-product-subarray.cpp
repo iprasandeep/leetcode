16118820253da7d2445ab7beed8c846c7b657833
prsndp <prasandeepp@gmail.com>
2022-09-08 22:12:07 +0530

    new problem slved in dp!

---
+++ DP/max-product-subarray.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int maxProduct(int a[], int n)
+{
+	int ans = a[0];
+	int ma = ans;
+	int mi = ans;
+	
+	for(int i = 1; i<n; i++)
+	{
+		if(a[i]<0)
+		{
+			swap(ma, mi);
+		}
+		ma = max(a[i], ma*a[i]);
+		mi = min(a[i], mi*a[i]);
+		ans = max(ans, ma); // max(2, ma)
+	}
+	return ans;
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	int a[n];
+	for(int i = 0; i<n; i++)
+	{
+		cin >> a[i];
+	}
+	cout<<maxProduct(a, n)<<"\n";
+return 0;
+}
